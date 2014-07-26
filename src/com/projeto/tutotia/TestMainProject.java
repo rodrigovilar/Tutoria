af6af7b89dd82f3d44476710de12acc8cc5790dd
Otaciso <Otaciso.silva@dce.ufpb.br>
2014-07-26 19:22:54 -0300

    Projeto iniciado

---
+++ src/com/projeto/tutotia/TestMainProject.java
@@ -0,0 +1,14 @@
+package com.projeto.tutotia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMainProject {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
